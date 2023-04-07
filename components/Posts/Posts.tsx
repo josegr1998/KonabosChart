b24526289f951160d614e825592433adbbd6da1c
Jose Raimondi <jose.gr98@yahoo.com>
2023-04-07 12:37:25 -0300

    -fixing build

--- components/Posts/Posts.tsx
+++ components/Posts/Posts.tsx
@@ -5,8 +5,8 @@ import { Container } from "../Container/Container";
 export const Posts = ({ data }: { data: IPostData[] }) => (
   <Container>
     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-8">
-      {data.map((post) => (
-        <Post data={post} />
+      {data.map((post,index) => (
+        <Post data={post} key={index} />
       ))}
     </div>
   </Container>
