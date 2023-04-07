85ed5a2a82969220189d4e0704d084a2312ba186
Jose Raimondi <jose.gr98@yahoo.com>
2023-04-07 12:26:39 -0300

    -Updated filter buttons style

--- components/Filter/Filter.tsx
+++ components/Filter/Filter.tsx
@@ -17,13 +17,13 @@ export const Filter = ({
       <div className='max-w-7xl m-auto mb-8 flex justify-center mt-6 md:mt-0 md:justify-start gap-x-4'>
         <div className='relative'>
           <button
-            className='rounded-md border-2 p-1 w-24 border-brandsPrimaryDark'
+            className='rounded-md border-2 p-1 px-2 w-36 border-brandsPrimaryDark'
             onClick={() => onDisplayChange("type")}
           >
             Type
           </button>
           <div
-            className={`transform transition-all absolute ${states.type.isOpen ? 'scale-100' : 'scale-0'} w-32 h-32 border border-brandsPrimary z-20 bg-white rounded-md
+            className={`transform transition-all absolute ${states.type.isOpen ? 'scale-100' : 'scale-0'} w-32 py-1 border border-brandsPrimary z-20 bg-white rounded-md
         mt-2 px-2`}
           >
             {filterFields.map(({ value, label }, index) => (
@@ -37,13 +37,13 @@ export const Filter = ({
         </div>
         <div className='relative'>
           <button
-            className='rounded-md border-2 p-1 w-24 border-brandsPrimaryDark'
+            className='rounded-md border-2 p-1 px-2 w-36 border-brandsPrimaryDark'
             onClick={() => onDisplayChange("date")}
           >
-            Date
+            Starting Date
           </button>
           <div
-            className={`transform transition-all absolute ${states.date.isOpen ? 'scale-100' : 'scale-0'} w-32 h-32 border border-brandsPrimary z-20 bg-white rounded-md
+            className={`transform transition-all absolute ${states.date.isOpen ? 'scale-100' : 'scale-0'} w-32 py-1 border border-brandsPrimary z-20 bg-white rounded-md
         mt-2 px-2`}
           >
             {filterDates.map((field, index) => (
