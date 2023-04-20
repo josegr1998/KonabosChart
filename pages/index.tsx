eacc0423c4999e4a51a5b6f4c76da3b8dc77d0b9
Jose Raimondi <jose.gr98@yahoo.com>
2023-04-19 22:26:43 -0300

    -removed unnecesary imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,4 @@
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import { GetServerSideProps } from "next";
-import { KenticoHttpRequest } from "clients/KenticoHttpRequest";
-import { KenticoParse } from "parsers/KenticoParse";
-import { getAuthorsCount } from "helpers/getAuthorsCount";
-import { IKenticoBlog } from "@/interfaces/kentico/IKenticoBlog";
-import { IITems } from "@/interfaces/IItems";
-import { IAuthorData } from "@/interfaces/app/IAuthorData";
-import { BarsChart } from "@/components/BarsChart/BarsChart";
 import { Homepage } from "@/components/Homepage/Homepage";
-import { IAuthor } from "@/interfaces/app/IAuthor";
 import { useHomepageData } from "hooks/useHomepageData";
 
 
